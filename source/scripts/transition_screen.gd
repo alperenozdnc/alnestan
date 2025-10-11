5eb0c80b4fff2760358eb68b65610478e084f763
alperenozdnc <alperenozdincc@gmail.com>
2025-10-11 22:48:49 +0300

    refactor(game): use better syntax option

--- source/scripts/transition_screen.gd
+++ source/scripts/transition_screen.gd
@@ -13,7 +13,7 @@ func transition():
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
-		emit_signal("transitioned");
+		transitioned.emit();
 		$animation_player.play("fade_to_normal");
 	if anim_name == "fade_to_normal":
 		$color_rect.visible = false;
