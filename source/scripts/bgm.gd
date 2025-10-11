1ebbe610f3fc44e0dc9769079c304b90bf3d814c
alperenozdnc <alperenozdincc@gmail.com>
2025-10-12 00:21:20 +0300

    feat(game): background music handler

---
+++ source/scripts/bgm.gd
@@ -0,0 +1,29 @@
+class_name bgm_manager extends AudioStreamPlayer
+
+var current_stream: String;
+
+func _ready() -> void:
+	alnestan.bgm = self;
+
+func play_stream(filename: String):
+	var path := "res://sounds/%s.mp3" % filename;
+
+	if not ResourceLoader.exists(path):
+		push_error("%s sound file does not exist on disk", path);
+		return;
+
+	self.stream = load(path);
+	self.play();
+
+
+func stop_stream():
+	var tween := create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE);
+	var duration_sec = 2.00;
+	var final_value = -80;
+
+	tween.tween_property(self, "volume_db", final_value, duration_sec);
+
+
+func set_tempo(n: float = 1):
+	self.pitch_scale = n;
+
--- source/scripts/global.gd
+++ source/scripts/global.gd
@@ -3,3 +3,4 @@ extends Node
 var scenes: scene_handler;
 var transitioner: transition_screen;
 var audio: audio_manager;
+var bgm: AudioStreamPlayer;
